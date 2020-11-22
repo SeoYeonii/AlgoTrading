498e2f50174ba739bb634ce744f5a6b4b6aa404c
SeoYeonii <jsy2015171@gmail.com>
2020-11-22 20:49:17 +0900

    new project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Title />
+    <router-view></router-view>
+
+  </div>
+</template>
+
+
+<script>
+import Title from './components/Home/Title'
+
+export default {
+  name: "app",
+  components: {
+    Title,
+  }
+}
+</script>
---
+++ src/components/Home/Title.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+      <img src="../../assets/img/algo_logo.png" alt="logo">
+  </div>
+</template>
+<script>
+export default {
+    name: 'Title'
+}
+</script>
+<style></style>
